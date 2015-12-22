7e01f117dedfd10497d8df501eb6204defbcd44b
Wallace Silva <ssouza.wallace@gmail.com>
2015-12-22 02:22:36 -0200

    Putting python interpreter and Logo Compiler

---
+++ Assets/Resources/IronPython/pyLogoCompiler/pyCricketEditor.py
@@ -0,0 +1,236 @@
+﻿#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#-----------------------------------------------------------------------------
+# Name: pyCricketLogo
+# Author: Marcelo Barbosa
+# Version: 0.1
+# About: Esta é uma versão do Cricket Logo em python para a placa Gogo board
+#
+#-----------------------------------------------------------------------------
+
+import pygtk
+pygtk.require('2.0')
+import gtk
+import pyYacc
+
+#from communication.Communication import Comm
+from pyLogoCompiler import pyYacc
+from pyLogoCompiler.Communication import GoGoComms
+
+global ERCP
+ERCP = ['one']
+
+class TextMode(object):
+    def showDialog(self,text,dialog_type,parent):
+        dialog = gtk.MessageDialog(parent, gtk.DIALOG_MODAL, dialog_type, gtk.BUTTONS_OK, text)
+        dialog.run()
+        dialog.destroy()
+    def showInfo(self,text,parent=None):
+        self.showDialog(text,gtk.MESSAGE_INFO,parent)
+    def showWarning(self,text,parent=None):
+        self.showDialog(text,gtk.MESSAGE_WARNING,parent)
+    def showError(self,text,parent=None):
+        self.showDialog(text,gtk.MESSAGE_ERROR,parent)
+        
+    def quit(self, widget):
+        self.window = None
+        self.GoGo.closePort()
+        del self
+
+    def __init__(self):
+        #Carrega a interface a partir do arquivo glade
+        
+        self.gui = gtk.glade.XML('gui/text.glade')
+        self.window = self.gui.get_widget('mainWindow')  
+        
+        self.GoGo = GoGoComms()
+
+        self.statusbar=self.gui.get_widget('statusbar')
+        self.gui.get_widget('statusbarVersion').push(0,'Versão '+VERSION)
+        self.notebookMain = self.gui.get_widget('notebookMain')    
+        self.liststore=gtk.ListStore(str,str,str) # Name, Unit, #Description
+        
+        self.sensorsTab     = SensorsTab(self.gui, self.liststore)
+        self.sensorTypes    = self.sensorsTab.sensorTypes        
+        self.proceduresTab  = ProceduresTab(self.gui, self.GoGo)
+        self.consoleTab     = ConsoleTab(self.gui, self.GoGo, self.statusbar, self.liststore, self.sensorTypes)
+        self.uploadTab      = UploadTab(self.gui, self.GoGo, self.liststore, self.sensorTypes)     
+        
+        self.notebookMain.set_current_page(0)    
+        self.notebookMain.set_show_tabs(True)    
+        
+        #Conecta Sinais aos Callbacks:        
+        dic = {"gtk_main_quit" : gtk.main_quit}
+        self.gui.signal_autoconnect(dic)    
+        self.gui.signal_autoconnect(self)
+        
+        self.window.connect("destroy", self.quit)
+        
+        #Exibe toda interface:
+        self.window.show_all() 
+        
+        # Tenta conectar-se à GogoBoard
+        if self.GoGo.autoConnect():
+            i = self.GoGo.getPort()
+            if os.name == 'nt':
+                self.statusbar.push(0,"Gogo connected on"+" COM "+str(i+1)+" "+"port")
+            else:
+                self.statusbar.push(0,"Gogo connected on"+" /dev/ttyS"+str(i)+" "+"port")
+            
+        else:
+            print "Gogo not found"
+            self.showWarning("There was a problem with the connection\nVerify that the board is properly connected with your computer")
+            self.window.destroy()
+            return
+            
+    def __init__(self):
+        window = gtk.Window(gtk.WINDOW_TOPLEVEL)
+        window.set_size_request(600, 600)
+        window.connect("destroy", self.close_application)
+        window.set_title("PyCricketLogo")
+        window.set_border_width(10)
+
+        box1 = gtk.VBox(False, 0)
+        window.add(box1)
+        box1.show()
+
+        label = gtk.Label("Codigo")    
+        label.set_alignment(0, 0)
+        box1.pack_start(label, False, False, 0)
+        label.show()
+
+        box2 = gtk.VBox(False, 0) 
+        box2.set_border_width(10)
+        box1.pack_start(box2, True, True, 0)
+        box2.show()
+
+        sw = gtk.ScrolledWindow()
+        sw.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
+        textview = gtk.TextView()
+        textbuffer = textview.get_buffer()
+        sw.add(textview)
+        sw.show()
+        textview.show()
+
+        box2.pack_start(sw)
+        # Load the file textview-basic.py into the text window
+        #infile = open("editor.py", "r")
+
+        #if infile:
+        #    string = infile.read()
+        #    infile.close()
+        #    textbuffer.set_text(string)
+
+        separator2 = gtk.HSeparator()
+        box1.pack_start(separator2, False, True, 0)
+        separator2.show()
+
+        label2 = gtk.Label("Resultado")    
+        label2.set_alignment(0, 0)
+        box1.pack_start(label2, False, False, 0)
+        label2.show()
+
+        box3 = gtk.HBox(False, 20)
+        box3.set_border_width(10)
+        box1.pack_start(box3, True, True, 0)
+        box3.show()
+
+        sw2 = gtk.ScrolledWindow()
+        sw2.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
+        textview2 = gtk.TextView()
+        textbuffer2 = textview2.get_buffer()
+        sw2.add(textview2)
+        sw2.show()
+        textview2.show()
+
+        box3.pack_start(sw2)
+
+        separator = gtk.HSeparator()
+        box1.pack_start(separator, False, True, 0)
+        separator.show()
+
+        box2 = gtk.HBox(False, 10)
+        box2.set_border_width(10)
+        box1.pack_start(box2, False, True, 0)
+        box2.show()
+
+        button = gtk.Button("Compilar")
+        button.connect("clicked", self.compilar, textbuffer, textbuffer2)
+        box2.pack_start(button, True, True, 0)        
+        button.show()
+        button1 = gtk.Button("Enviar")
+        button1.connect("clicked", self.enviarGogo, textbuffer, textbuffer2)
+        box2.pack_start(button1, True, True, 0)        
+        button1.show()
+        button2 = gtk.Button("Fechar")
+        button2.connect("clicked", self.close_application)
+        box2.pack_start(button2, True, True, 0)
+        button2.set_flags(gtk.CAN_DEFAULT)
+        button2.grab_default()
+        button2.show()
+        
+        window.show()            
+    
+    def toggle_editable(self, checkbutton, textview):
+        textview.set_editable(checkbutton.get_active())
+
+    def toggle_cursor_visible(self, checkbutton, textview):
+        textview.set_cursor_visible(checkbutton.get_active())
+
+    def toggle_left_margin(self, checkbutton, textview):
+        if checkbutton.get_active():
+            textview.set_left_margin(50)
+        else:
+            textview.set_left_margin(0)
+
+    def toggle_right_margin(self, checkbutton, textview):
+        if checkbutton.get_active():
+            textview.set_right_margin(50)
+        else:
+            textview.set_right_margin(0)
+
+    def new_wrap_mode(self, radiobutton, textview, val):
+        if radiobutton.get_active():
+            textview.set_wrap_mode(val)
+
+    def new_justification(self, radiobutton, textview, val):
+        if radiobutton.get_active():
+            textview.set_justification(val)
+
+    def close_application(self, widget):
+        gtk.main_quit()
+
+    def compilar(self, widget, textbuffer, textbuffer2):
+        compilado = '%s ' % self.ler_token(widget, textbuffer, textbuffer2)
+        if  len(compilado) <= 5 :
+            global ERCP
+            #print "CCCCCCCCCCCCCCCCOMPILOU COM ERRRO  ?"
+            textbuffer2.set_text("ERROR->Look for a text containing one of the words: " + ERCP[0] )
+            #print "????????erroCOMP"
+            #print ERCP[0]           
+            #print "????????ERCP"
+
+            
+        else :
+            textbuffer2.set_text(compilado)
+
+    def enviarGogo(self, widget, textbuffer, textbuffer2):
+        codigo = self.ler_token(widget, textbuffer, textbuffer2)
+        c = pyCricketComunic.Comunic(0)    
+        result = c.enviar(codigo)
+        c.fechaPorta()
+        dialog = gtk.MessageDialog(None, gtk.DIALOG_MODAL, gtk.MESSAGE_INFO,
+        gtk.BUTTONS_NONE, result)
+        dialog.add_button(gtk.STOCK_CLOSE, gtk.RESPONSE_CLOSE)
+        dialog.run()
+        dialog.destroy()
+        
+    def ler_token(self, widget, textbuffer, textbuffer2):
+        global ERCP
+        start = textbuffer.get_start_iter()
+        end = textbuffer.get_end_iter()
+        texto = pyYacc.analisarCodigo(textbuffer.get_text(start, end),ERCP)
+        print "ERCP", ERCP[0]
+        return texto        
+        #for i in texto:
+        #    armazena = armazena + str(i) + '\n'
---
+++ Assets/Scripts/Programming/PythonEditorWindow.cs
@@ -0,0 +1,247 @@
+﻿using UnityEngine;  
+using UnityEditor;  
+using IronPython;  
+using IronPython.Modules;  
+using System.Text;  
+using System.Collections.Generic;  
+using Microsoft.Scripting.Hosting;  
+
+// derive from EditorWindow for convenience, but this is just a fire-n-forget script  
+public class PythonEditorWindow : EditorWindow  
+{  
+	// class member properties
+	Vector2 _historyScroll;  
+	Vector2 _scriptScroll;  
+	bool _showHistory = true;  
+	int _historyPaneHeight = 192;  
+	string _historyText = "history";  
+	string _scriptText = "script";  
+	string _lastResult = "";  
+	TextEditor _TEditor;  
+	GUIStyle consoleStyle = new GUIStyle ();  
+	GUIStyle historyStyle = new GUIStyle ();  
+	Microsoft.Scripting.Hosting.ScriptEngine _ScriptEngine;  
+	Microsoft.Scripting.Hosting.ScriptScope _ScriptScope;  
+
+	[MenuItem ("Python/LogoCompileTest")]
+	public static void LogoCompileTest () {
+		// create the engine  
+		var ScriptEngine = IronPython.Hosting.Python.CreateEngine();  
+		// and the scope (ie, the python namespace)  
+		var ScriptScope = ScriptEngine.CreateScope();  
+
+		// execute a string in the interpreter and grab the variable  
+		string dllpath = System.IO.Path.GetDirectoryName (  
+		                                                  (typeof(ScriptEngine)).Assembly.Location).Replace (  
+		                                                   "\\", "/");  
+		StringBuilder example = new StringBuilder();  
+		example.AppendLine ("import sys");  
+		example.AppendFormat ("sys.path.append(\"{0}\")\n", dllpath + "/Lib");  
+		example.AppendFormat ("sys.path.append(\"{0}\")\n", dllpath + "/DLLs");  
+		example.AppendFormat ("sys.path.append(\"{0}\")\n", dllpath + "/pyLogoCompiler");
+		example.AppendLine("import Compiler");  
+		example.AppendLine("output, ERCP = Compiler.compile()");  
+
+		var ScriptSource = ScriptEngine.CreateScriptSourceFromString(example.ToString());  
+		ScriptSource.Execute(ScriptScope);  
+
+		IronPython.Runtime.List came_from_script = ScriptScope.GetVariable<IronPython.Runtime.List>("output");  
+
+		IronPython.Runtime.List ERCP = ScriptScope.GetVariable<IronPython.Runtime.List>("ERCP");  
+
+		Debug.Log("Saida: ");
+		if (came_from_script != null) {
+			string toLog = "[";
+			foreach (var elem in came_from_script) {
+				toLog += elem.ToString() + " ";
+			}
+			toLog += "]";
+			Debug.Log(toLog);
+		}
+		if (ERCP != null) {
+			string toLog = "[";
+			foreach (var elem in ERCP) {
+				toLog += elem.ToString() + " ";
+			}
+			toLog += "]";
+			Debug.Log(toLog);
+		}
+
+	}
+
+	[MenuItem ("Python/PythonEditorWindow")]
+	static void Init () {
+		// Get existing open window or if none, make a new one:
+		PythonEditorWindow window = (PythonEditorWindow)EditorWindow.GetWindow (typeof (PythonEditorWindow));
+		window.Show();
+	}
+
+	// initialization logic (it's Unity, so we don't do this in the constructor!
+	public void OnEnable ()  
+	{     
+		// pure gui stuff
+		consoleStyle.normal.textColor = Color.yellow;  
+		consoleStyle.margin = new RectOffset (20, 10, 10, 10);  
+		historyStyle.normal.textColor = Color.white;  
+		historyStyle.margin = new RectOffset (20, 10, 10, 10);  
+		
+		// load up the hosting environment  
+		var options = new Dictionary<string, object>();
+		options["Frames"] = true;
+		options["FullFrames"] = true;
+		_ScriptEngine = IronPython.Hosting.Python.CreateEngine (options);  
+		_ScriptScope = _ScriptEngine.CreateScope ();  
+		
+		// load the assemblies for unity, using types  
+		// to resolve assemblies so we don't need to hard code paths  
+		_ScriptEngine.Runtime.LoadAssembly (typeof(PythonFileIOModule).Assembly);  
+		_ScriptEngine.Runtime.LoadAssembly (typeof(GameObject).Assembly);  
+		_ScriptEngine.Runtime.LoadAssembly (typeof(Editor).Assembly);  
+		string dllpath = System.IO.Path.GetDirectoryName (  
+		                                                  (typeof(ScriptEngine)).Assembly.Location).Replace (  
+		                                                   "\\", "/");  
+		// load needed modules and paths  
+		StringBuilder init = new StringBuilder ();  
+		init.AppendLine ("import sys");  
+		init.AppendFormat ("sys.path.append(\"{0}\")\n", dllpath + "/Lib");  
+		init.AppendFormat ("sys.path.append(\"{0}\")\n", dllpath + "/DLLs");  
+		init.AppendFormat ("sys.path.append(\"{0}\")\n", dllpath + "/pyLogoCompiler");
+		init.AppendLine ("import UnityEngine as unity");  
+		init.AppendLine ("import UnityEditor as editor");  
+		init.AppendLine ("import StringIO");  
+		init.AppendLine ("unity.Debug.Log(\"Python console initialized\")");  
+		init.AppendLine ("__print_buffer = sys.stdout = StringIO.StringIO()");  
+		var ScriptSource = _ScriptEngine.CreateScriptSourceFromString (init.ToString ());  
+		ScriptSource.Execute (_ScriptScope);  
+	}   
+	
+	public void OnGUI ()  
+	{  
+		HackyTabSubstitute ();  // this is explained below...
+		
+		// top pane with history  
+		_showHistory = EditorGUILayout.Foldout (_showHistory, "History");  
+		if (_showHistory) {  
+			EditorGUILayout.BeginVertical (GUILayout.ExpandWidth (true),   
+			                               GUILayout.Height (_historyPaneHeight));  
+			if (GUILayout.Button ("Clear history")) {  
+				_historyText = "";  
+			}  
+			_historyScroll = EditorGUILayout.BeginScrollView (_historyScroll);  
+			EditorGUILayout.TextArea (_historyText,   
+			                          historyStyle,   
+			                          GUILayout.ExpandWidth (true),   
+			                          GUILayout.ExpandHeight (true));          
+			EditorGUILayout.EndScrollView ();  
+			EditorGUILayout.EndVertical ();  
+		}  
+		// draggable splitter  
+		GUILayout.Box ("", GUILayout.Height (8), GUILayout.ExpandWidth (true));  
+		//Lower pane for script editing  
+		EditorGUILayout.BeginVertical (GUILayout.ExpandWidth (true),   
+		                               GUILayout.ExpandHeight (true));  
+		_scriptScroll = EditorGUILayout.BeginScrollView (_scriptScroll);  
+		GUI.SetNextControlName ("script_pane");  
+		// note use of GUILayout NOT EditorGUILayout.  
+		// TextEditor is not accessible for EditorGUILayout!  
+		_scriptText = GUILayout.TextArea (_scriptText,   
+		                                  consoleStyle,  
+		                                  GUILayout.ExpandWidth (true),   
+		                                  GUILayout.ExpandHeight (true));          
+		_TEditor = (TextEditor)GUIUtility.GetStateObject (typeof(TextEditor), GUIUtility.keyboardControl);  
+		EditorGUILayout.EndScrollView ();  
+		EditorGUILayout.BeginHorizontal ();  
+		if (GUILayout.Button("Clear", GUILayout.ExpandWidth(true)))  
+		{  
+			_scriptText = "";  
+			GUI.FocusControl("script_pane");  
+		}  
+		if (GUILayout.Button ("Execute and clear", GUILayout.ExpandWidth (true))) {  
+			Intepret (_scriptText);  
+			_scriptText = "";  
+			GUI.FocusControl("script_pane");  
+		}  
+		if (GUILayout.Button ("Execute", GUILayout.ExpandWidth (true))) {  
+			Intepret (_scriptText);  
+		}  
+		EditorGUILayout.EndHorizontal ();  
+		EditorGUILayout.EndVertical ();      
+		// mimic maya Ctrl+enter = execute  
+		if (Event.current.isKey &&  
+		    Event.current.keyCode == KeyCode.Return &&  
+		    Event.current.type == EventType.KeyUp &&  
+		    Event.current.control) {  
+			Intepret (_scriptText);  
+		}  
+		// drag the splitter  
+		if (Event.current.isMouse & Event.current.type == EventType.mouseDrag)  
+		{_historyPaneHeight = (int) Event.current.mousePosition.y - 28;  
+			Repaint();  
+		}  
+	}  
+
+	private void HackyTabSubstitute ()  
+	{  
+		string _t = _scriptText;  
+		string[] lines = _scriptText.Split ('\n');  
+		for (int i = 0; i< lines.Length; i++) {  
+			if (lines [i].IndexOf ('`') >= 0) {  
+				lines [i] = "  " + lines [i];  
+//				_TEditor.select.selectPos = _TEditor.pos = _TEditor.pos + 3;  
+			}  
+			if (lines [i].IndexOf ("  ") >= 0 && lines [i].IndexOf ("~") >= 0) {  
+				if (lines [i].StartsWith ("  "))  
+					lines [i] = lines [i].Substring (4);  
+//				_TEditor.selectPos = _TEditor.pos = _TEditor.pos - 4;  
+			}  
+			lines [i] = lines [i].Replace ("~", "");  
+			lines [i] = lines [i].Replace ("`", "");  
+		}  
+		_scriptText = string.Join ("\n", lines);  
+		if (_scriptText != _t)  
+			Repaint ();  
+	}  
+
+	// Pass the script text to the interpreter and display results  
+	private void Intepret (string text_to_interpret)  
+	{  
+		object result = null;  
+		try {  
+			Undo.RegisterSceneUndo ("script");  
+			var scriptSrc = _ScriptEngine.CreateScriptSourceFromString (text_to_interpret);  
+			_historyText += "\n";  
+			_historyText += text_to_interpret;  
+			_historyText += "\n";  
+			result = scriptSrc.Execute (_ScriptScope);  
+		}   
+		// Log exceptions to the console too  
+		catch (System.Exception e) {  
+			Debug.LogException (e);  
+			_historyText += "\n";  
+			_historyText += "#  " + e.Message + "\n";  
+		}   
+		finally {  
+			// grab the __print_buffer stringIO and get its contents  
+			var print_buffer = _ScriptScope.GetVariable ("__print_buffer");  
+			var gv = _ScriptEngine.Operations.GetMember (print_buffer, "getvalue");  
+			var st = _ScriptEngine.Operations.Invoke (gv);  
+			var src = _ScriptEngine.CreateScriptSourceFromString ("__print_buffer = sys.stdout = StringIO.StringIO()");  
+			src.Execute (_ScriptScope);  
+			if (st.ToString ().Length > 0) {  
+				_historyText += "";  
+				foreach (string l in st.ToString().Split('\n'))  
+				{  
+					_historyText += "  " + l + "\n";  
+				}  
+				_historyText += "\n";  
+			}  
+			// and print the last value for single-statement evals  
+			if (result != null) {  
+				_historyText += "#  " + result.ToString () + "\n";  
+			}  
+			int lines = _historyText.Split ('\n').Length;  
+			_historyScroll.y += (lines * 19);                  
+			Repaint ();  
+		}  
+	}  
+}  
