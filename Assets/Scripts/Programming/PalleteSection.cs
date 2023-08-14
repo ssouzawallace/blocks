d72c76efb0176c1e4ca4d21ae5a8bd2d2c9c47f3
Wallace Souza Silva <wallace@wcorp.tech>
2023-08-14 20:04:00 -0300

    Merge main back to develop (#20)
    * Fix (#17)
    
    * Fix merge conflicts.
    
    * Fix script refereneces
    
    ---------
    
    Co-authored-by: S. Wallace <wallace@wcor.tech>
    
    * Develop (#18)
    
    * Testing readme
    
    * Change in the architecture of the blocks Prefabs and automatic generations of the sections
    
    * Adding code generator in Block.cs
    
    * Testing with a few more blocks
    
    * Modifying blocks structure in order to improve scalability and reuse
    
    * Continuing last commit
    
    * More of the same
    
    * Testing expansive blocks
    
    * math operations
    
    * Expansive If Block and blocks with procedure. But the result isn't that good yet
    
    * Flow control Expansive Blocks
    
    * Fixing the Math operations connections
    
    * A bunch of new blocks and explicit trash area.
    
    * Putting python interpreter and Logo Compiler
    
    * Removing the readme from the Dev branch
    
    * Adding sensor, start block and fixing some bugs
    
    * 🌈
    
    * yes
    
    * Update README.md
    
    * Add files via upload
    
    * Create README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Unity Editor version bump
    
    * Update README.md
    
    * Update README.md
    
    * Update minusnode.gif.meta
    
    * Update ConditionOperatorBlock.cs.meta
    
    * Update PythonEditorWindow.cs
    
    * Update .gitignore
    
    * Cleaning up IronPython.
    
    * Update README.md
    
    ---------
    
    Co-authored-by: S. Wallace <wallace@wcor.tech>
    
    * Fix (#19)
    
    * Fix merge conflicts.
    
    * Fix script refereneces
    
    * Fix typo
    
    * Fix prefab vertical layout
    
    ---------
    
    Co-authored-by: S. Wallace <wallace@wcor.tech>
    
    ---------
    
    Co-authored-by: S. Wallace <wallace@wcor.tech>

--- Assets/Scripts/Programming/PalleteSection.cs
+++ Assets/Scripts/Programming/PalleteSection.cs
@@ -8,20 +8,20 @@ public class PalleteSection : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		float totalHeight = 0.0f;
-		int cont = 0;
+		int count = 0;
 		LayoutElement[] childrenLayoutElements = this.gameObject.GetComponentsInChildren<LayoutElement> ();
 		
 		foreach (LayoutElement layoutElement in childrenLayoutElements) {
 			if (layoutElement.transform.parent == this.transform) {
 				totalHeight += layoutElement.minHeight;
-				cont++;
+				count++;
 			}
 		}
 
 		VerticalLayoutGroup verticalLayGroup = GetComponent <VerticalLayoutGroup> ();
 		totalHeight += verticalLayGroup.padding.top;
-		totalHeight += verticalLayGroup.spacing * (cont);
+		totalHeight += verticalLayGroup.spacing * (count);
 
-		GetComponent<RectTransform> ().sizeDelta = new Vector2(0, totalHeight);
+		GetComponent<RectTransform>().sizeDelta = new Vector2(0, totalHeight);
 	}
 }
